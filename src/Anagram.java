2e0e9c91a8c6e729b0c2ee6e8e1e263ef7b2a773
kirken <kirke.narusk@itcollege.ee>
2019-03-03 20:23:19 +0200

    AnagramSolver ver2 hashtable

--- src/Anagram.java
+++ src/Anagram.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Anagram {
 
+    //HOMEMADE HASHTABLE
     public static void main(String[] args) throws IOException {
         String pathname = args[0];
         String word = args[1];
@@ -11,15 +12,41 @@ public class Anagram {
         long startTime = System.nanoTime();
         File file = new File(pathname);
         StringBuilder result = new StringBuilder();
+        word = word.toLowerCase();
 
-        //read from file BUFFEREDREADER
+        Hashtable<Character, Double> hash = new Hashtable<Character,Double>();
+        hash.put('a', 2.0);hash.put('b', 3.0);hash.put('c', 5.0);hash.put('d', 7.0);
+        hash.put('e', 11.0);hash.put('f', 13.0);hash.put('g', 17.0);hash.put('h', 19.0);
+        hash.put('i', 23.0);hash.put('j', 29.0);hash.put('k', 31.0);hash.put('l', 37.0);
+        hash.put('m', 41.0);hash.put('n', 43.0);hash.put('o', 47.0);hash.put('p', 53.0);
+        hash.put('q', 59.0);hash.put('r', 61.0);hash.put('s', 67.0);hash.put('t', 71.0);
+        hash.put('u', 73.0);hash.put('v', 79.0);hash.put('w', 83.0);hash.put('x', 89.0);
+        hash.put('y', 97.0);hash.put('z', 101.0);hash.put('õ', 103.0);hash.put('ä', 107.0);
+        hash.put('ö', 109.0);hash.put('ü', 113.0);hash.put('š', 1127.0);hash.put('ž', 131.0);
+        hash.put(' ', 137.0);
+        double code = 1.0;
+        for(Character ch : word.toCharArray()) {
+            code*=hash.get(ch);
+        }
+        double n; //to hold input word's code value
+
+        //read from bufferreader
         FileInputStream fileInputStream = new FileInputStream(file);
         InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
 
         try (BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                if (isAnagram(word, line)) {
+                if (word.length() != line.length())
+                    continue;
+                line = line.toLowerCase();
+                n = code; //input word's hashcode
+                for(Character ch : line.toCharArray()) {
+                    if (hash.get(ch) != null){
+                        n/=hash.get(ch);
+                    }
+                }
+                if (n==1.0) {
                     result.append(", ");
                     result.append(line);
                 }
@@ -29,50 +56,5 @@ public class Anagram {
         long timeElapsed = (endTime - startTime) / 1000; //nanotime to microtime
         System.out.print(timeElapsed);
         System.out.println(result);
-    }
-
-
-    private static boolean isAnagram(String a, String b) {
-
-        if (a == null || b == null || a.equals("") || b.equals(""))
-            throw new IllegalArgumentException();
-
-        //testing lenght
-        if (a.length() != b.length())
-            return false;
-
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-
-        // populate a map with letters and frequencies of String b
-        Map<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < b.length(); i++) {
-            char letter = b.charAt(i);
-
-            if (!map.containsKey(letter)) {
-                map.put(letter, 1);
-            } else {
-                Integer frequency = map.get(letter);
-                map.put(letter, ++frequency);
-            }
-        }
-
-        //test letters in String a against letters in String b map
-        for (int i = 0; i < a.length(); i++) {
-            char letter = a.charAt(i);
-
-            if (!map.containsKey(letter))
-                return false;
-
-            Integer frequency = map.get(letter);
-
-            if (frequency == 0)
-                return false;
-            else
-                map.put(letter, --frequency);
-        }
-
-        return true;
     }
 }
